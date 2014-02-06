f91bc7c32ed24f94ff261fc7491d16dc14471065
Kevin Hartman <kevin@hart.mn>
2014-02-06 00:21:28 -0500

    Added iOS target.'

--- LiquidKitTests/LiquidKitTests.m
+++ LiquidKitTests/LiquidKitTests.m
@@ -2,7 +2,7 @@
 //  LiquidKitTests.m
 //  LiquidKitTests
 //
-//  Created by Kevin Hartman on 2/5/14.
+//  Created by Kevin Hartman on 2/6/14.
 //  Copyright (c) 2014 Kevin Hartman. All rights reserved.
 //
 
