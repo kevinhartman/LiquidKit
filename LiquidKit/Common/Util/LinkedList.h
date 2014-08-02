52371f2aaefb9751a871e1d0c6a21322793c5706
Kevin Hartman <kevin@hart.mn>
2014-08-01 23:27:16 -0700

    Created a linked list implementation that should be pretty quick for the purposes of my rasterization optimizations.

---
+++ LiquidKit/Common/Util/LinkedList.h
@@ -0,0 +1,27 @@
+//
+//  LinkedList.h
+//  LiquidKit
+//
+//  Created by Kevin Hartman on 8/1/14.
+//  Copyright (c) 2014 Kevin Hartman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+struct Element {
+    __unsafe_unretained id reference;
+    struct Element *next;
+};
+typedef struct Element Element;
+
+@interface LinkedList : NSObject
+
+@property NSArray *data;
+@property Element *list;
+@property Element *head;
+
+- (id) initWithArray:(NSArray *)input;
+
+- (id) remove:(BOOL (^) (id obj, NSUInteger idx))condition;
+
+@end
---
+++ LiquidKit/Common/Util/LinkedList.m
@@ -0,0 +1,67 @@
+//
+//  LinkedList.m
+//  LiquidKit
+//
+//  Created by Kevin Hartman on 8/1/14.
+//  Copyright (c) 2014 Kevin Hartman. All rights reserved.
+//
+
+#import "LinkedList.h"
+
+@implementation LinkedList
+
+
+- (id) initWithArray:(NSArray *)input {
+    
+    if (self = [super init]) {
+        _data = input;
+        _list = malloc(sizeof(Element) * [input count]);
+        _head = _list;
+        
+        [input enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            
+            _list[idx].reference = obj;
+            _list[idx].next = (idx == [input count] - 1) ? NULL : &_list[idx + 1];
+            
+        }];
+    }
+    
+    return self;
+}
+
+- (id) remove:(BOOL (^) (id obj, NSUInteger idx))condition {
+    
+    NSUInteger index = 0;
+    
+    Element *iter = _head;
+    Element *prev = NULL;
+    
+    while (iter) {
+        
+        if (condition(iter->reference, index)) {
+            
+            id reference = iter->reference;
+            
+            if (index == 0) {
+                _head = iter->next;
+                return reference;
+            }
+            
+            prev->next = iter->next;
+            return reference;
+        }
+        
+        index++;
+        prev = iter;
+        iter = iter->next;
+    }
+    
+    return nil;
+}
+
+-(void)dealloc {
+    free(_list);
+}
+
+
+@end
